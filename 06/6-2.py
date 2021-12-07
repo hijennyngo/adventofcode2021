b3789c441962a655dfa71492596cde504c6447bb
Jenny Ngo <90799858+hijennyngo@users.noreply.github.com>
2021-12-06 21:23:13 -0800

    Update 6-2.py

--- 06/6-2.py
+++ 06/6-2.py
@@ -1,13 +1,37 @@
-# LOL this takes too long
-
 file = open('6.txt')
 content = file.read()
 lanternfish = map(int, content.split(","))
+lf_dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+for fish in lanternfish:
+  if fish == 0:
+    lf_dict[0] += 1
+  elif fish == 1:
+    lf_dict[1] += 1
+  elif fish == 2:
+    lf_dict[2] += 1
+  elif fish == 3:
+    lf_dict[3] += 1
+  elif fish == 4:
+    lf_dict[4] += 1
+  elif fish == 5:
+    lf_dict[5] += 1
+  elif fish == 6:
+    lf_dict[6] += 1
+  elif fish == 7:
+    lf_dict[7] += 1
+  elif fish == 8:
+    lf_dict[8] += 1
 for i in range(256):
-  for index, fish in enumerate(lanternfish):
-    if fish == 0:
-      lanternfish[index] = 6
-      lanternfish.append(9)
-    else:
-      lanternfish[index] -= 1
-print(len(lanternfish))
+  lf_dict[7] += lf_dict[0]
+  lf_dict[9] += lf_dict[0]
+  lf_dict[0] = lf_dict[1]
+  lf_dict[1] = lf_dict[2]
+  lf_dict[2] = lf_dict[3]
+  lf_dict[3] = lf_dict[4]
+  lf_dict[4] = lf_dict[5]
+  lf_dict[5] = lf_dict[6]
+  lf_dict[6] = lf_dict[7]
+  lf_dict[7] = lf_dict[8]
+  lf_dict[8] = lf_dict[9]
+  lf_dict[9] = 0
+print(sum(lf_dict.values()))
